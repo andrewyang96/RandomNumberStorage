2da7cd38c2feb6caa62e9fffa48a66fc261886ee
Andrew Yang <andrewyang96@gmail.com>
2018-01-07 22:03:59 -0600

    Flesh out code

--- app.py
+++ app.py
@@ -1,16 +1,43 @@
+from datetime import datetime
 from flask import Flask
+import json
+import psycopg2
 import logging
 import random
+
+with open('config.json', 'r') as f:
+    config = json.load(f)
 
 app = Flask(__name__)
 logging.basicConfig()
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
+def get_db_connection():
+    """Get psycopg2 connection to RDS instance."""
+    db_config = config['database']
+    return psycopg2.connect(
+        dbname=db_config['db_name'],
+        user=db_config['username'],
+        host=db_config['host_url'])
+
 @app.route('/', methods=['GET'])
 def main_handler(event=None, context=None):
-    random_num = random.randint(0, 9)
-    logger.info('Random number is {0}'.format(random_num))
+    table_name = config['database']['table_name']
+    conn = get_db_connection()
+    cur = conn.cursor()
+
+    random_num = random.randint(0, config['max_number'])
+    current_dt = datetime.utcnow()
+
+    cur.execute(
+        'INSERT INTO %s (timestamp, random_num) VALUES (%s, %s)',
+        (table_name, current_dt, random_num))
+    conn.commit()
+    conn.close()
+
+    logger.info(
+        '{0}: Inserted random number {1}'.format(current_dt, random_num))
     return str(random_num)
 
 if __name__ == '__main__':

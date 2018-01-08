816e0a329934fe336235cc4d403d17975a9bc340
Andrew Yang <andrewyang96@gmail.com>
2018-01-07 18:37:08 -0600

    Rename script and zappa init

--- main.py
+++ main.py
@@ -8,19 +8,20 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 DB_NAME = 'random_numbers'
-HOST_URL = os.environ['HOST_URL']  # to be set in Lambda
-TABLE_NAME = os.environ['TABLE_NAME']  # to be set in Lambda
-USERNAME = os.environ['USERNAME']  # to be set in Lambda
 
+# Lambda environment variables
+HOST_URL = os.environ.get('HOST_URL')
+TABLE_NAME = os.environ.get('TABLE_NAME')
+USERNAME = os.environ.get('USERNAME')
+EMAIL_ADDRESS = os.environ.get('EMAIL_ADDRESS')
 
-def get_connection():
+def get_db_connection():
     """Get psycopg2 connection to RDS instance."""
     return psycopg2.connect(dbname=DB_NAME, user=USERNAME, host=HOST_URL)
 
-
 def run(event, context):
     """Primary Lambda handler."""
-    conn = get_connection()
+    conn = get_db_connection()
     cur = conn.cursor()
     random_num = random.randint(-2 ** 31, 2 ** 31 + 1)
     current_dt = datetime.utcnow()
@@ -31,3 +32,6 @@ def run(event, context):
     conn.close()
     logger.info(
         '{0}: Inserted random number {1}'.format(current_dt, random_num))
+    if EMAIL_ADDRESS:
+        # TODO: send email
+        logger.info('Sent email to {0}'.format(EMAIL_ADDRESS))

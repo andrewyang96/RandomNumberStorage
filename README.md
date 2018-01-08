f907e7da758c9bd232bddb7e9fc5b87666d84880
Andrew Yang <andrewyang96@gmail.com>
2018-01-07 23:11:34 -0600

    Ditch zappa use python-lambda

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # RandomNumberStorage
-Test application using AWS Lambda, SES, and RDS (Postgres).
+Test application using AWS Lambda, SES, and RDS (Postgres). Deployed with [python-lambda](https://github.com/nficano/python-lambda).
+
+## Development
+Make a virtual environment and run `pip install -r requirements.txt`.
+
+## Environment variables
+
+## Deployment
--- service.py
+++ service.py
@@ -1,31 +1,22 @@
 from datetime import datetime
-from flask import Flask
 import json
 import psycopg2
-import logging
 import random
 
 with open('config.json', 'r') as f:
     config = json.load(f)
 
-app = Flask(__name__)
-logging.basicConfig()
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-
 def get_db_connection():
     """Get psycopg2 connection to RDS instance."""
     db_config = config['database']
-    logger.info('Connecting to database at {0}'.format(db_config['host_url']))
+    print('Connecting to database at {0}'.format(db_config['host_url']))
     return psycopg2.connect(
         host=db_config['host_url'],
         dbname=db_config['db_name'],
         user=db_config['username'],
         password=db_config['password'])
 
-@app.route('/', methods=['GET'])
-def main_handler(event=None, context=None):
-    logger.info('Handler called!')
+def handler(event, context):
     conn = get_db_connection()
     cur = conn.cursor()
 
@@ -39,9 +30,5 @@ def main_handler(event=None, context=None):
     cur.close()
     conn.close()
 
-    logger.info(
-        '{0}: Inserted random number {1}'.format(current_dt, random_num))
-    return str(random_num)
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    print('{0}: Inserted random number {1}'.format(current_dt, random_num))
+    return random_num

4d3383a640a54c5a74cafe44fedd0d38e55beb4a
Andrew Yang <andrewyang96@gmail.com>
2018-01-07 21:27:52 -0600

    Try using Flask

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+import logging
+import random
+
+app = Flask(__name__)
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+@app.route('/', methods=['GET'])
+def main_handler(event=None, context=None):
+    random_num = random.randint(0, 9)
+    logger.info('Random number is {0}'.format(random_num))
+    return random_num
+
+if __name__ == '__main__':
+    app.run(debug=True)

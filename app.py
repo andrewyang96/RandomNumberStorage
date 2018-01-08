5f69b349c9908bbb87738ef9377dfece5b8cad5b
Andrew Yang <andrewyang96@gmail.com>
2018-01-07 21:35:46 -0600

    Don't return int

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ logger.setLevel(logging.DEBUG)
 def main_handler(event=None, context=None):
     random_num = random.randint(0, 9)
     logger.info('Random number is {0}'.format(random_num))
-    return random_num
+    return str(random_num)
 
 if __name__ == '__main__':
     app.run(debug=True)

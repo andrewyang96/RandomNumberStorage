615899e36638421b2f13725ebf19b689111e80ce
Andrew Yang <andrewyang96@gmail.com>
2018-01-08 22:35:41 -0600

    Implement emailing

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Test application using AWS Lambda, SES, and RDS (Postgres). Deployed with [python-lambda](https://github.com/nficano/python-lambda).
 
 ## Initialization
-Make an AWS Lambda function named "random_numbers". Set its handler to `service.handler` and save changes.
+Make an AWS Lambda function named "random_numbers". Set its handler to `service.handler` and save changes. Then, verify an email on Amazon SES.
 
 ## Environment variables
 Run `cp config.json.example config.json` and fill in the fields.
@@ -11,4 +11,4 @@ Run `cp config.json.example config.json` and fill in the fields.
 Download [awslambda-psycopg2](https://github.com/jkehler/awslambda-psycopg2) and copy the `psycopg2-3.6` into the project directory. Rename it to `psycopg2`.
 
 ## Deployment
-Run `make deploy`. Configure a CloudWatch Event and its schedule expression, and save changes.
+Run `make deploy`. Configure a CloudWatch Event and its schedule expression, and save changes. Set the "ENABLE_EMAIL" environment variable in the Lambda console and add the "AmazonSESFullAccess" permission to the execution role to enable email sending.
--- service.py
+++ service.py
@@ -1,5 +1,8 @@
+import boto3
+from botocore.exceptions import ClientError
 from datetime import datetime
 import json
+import os
 import psycopg2
 import random
 
@@ -15,6 +18,48 @@ def get_db_connection():
         dbname=db_config['db_name'],
         user=db_config['username'],
         password=db_config['password'])
+
+def send_email(timestamp, random_num):
+    """Send email via Amazon SES."""
+    email_config = config['email']
+    subject = 'Random Number Storage Test Update'
+    message = 'The random number is {0}. Generated at {1} UTC'.format(
+        random_num, timestamp.strftime('%Y-%m-%d %H:%M:%S'))
+    body_html = '''<html>
+        <head></head><body>
+            <p>{0}</p>
+            <img src="https://www.python.org/static/community_logos/python-logo.png">
+        </body>
+    </html>'''.format(message)
+    client = boto3.client('ses', region_name='us-east-1')
+    try:
+        response = client.send_email(
+            Destination={
+                'ToAddresses': [
+                    email_config['recipient'],
+                ],
+            }, Message={
+                'Body': {
+                    'Html': {
+                        'Charset': 'UTF-8',
+                        'Data': body_html,
+                    },
+                    'Text': {
+                        'Charset': 'UTF-8',
+                        'Data': message,
+                    },
+                },
+                'Subject': {
+                    'Charset': 'UTF-8',
+                    'Data': subject,
+                },
+            },
+            Source=email_config['sender']
+        )
+    except ClientError as e:
+        print('Error sending email: {0}'.format(e.response['Error']['Message']))
+    else:
+        print('Email sent! Message ID: {0}'.format(response['ResponseMetadata']['RequestId']))
 
 def handler(event, context):
     conn = get_db_connection()
@@ -32,4 +77,9 @@ def handler(event, context):
     conn.close()
 
     print('{0}: Inserted random number {1}'.format(current_dt, random_num))
+    if os.environ.get('ENABLE_EMAIL'):
+        print('Email enabled, sending email')
+        send_email(current_dt, random_num)
+    else:
+        print('Email disabled')
     return random_num
